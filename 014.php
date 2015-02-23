f7791e35fad5f33087ed48ad66a6a4f9bd91b483
davidyzn <davidyznbak@hotmail.com>
2015-02-23 23:28:38 +0800

    update

---
+++ 014.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<?php
+echo "今天是".date("Y/m/d")."<br>";
+echo "今天是".date("Y.m.d")."<br>";
+echo "今天是".date("Y-m-d")."<br>";
+echo "今天是".date("1");
+?>
+</body>
+</html>
---
+++ 016.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<h1>欢迎访问我们的首页！</h1>
+<p>一段文本。</p>
+<p>一段文本。</p>
+
+<?php 
+include '../015.php';
+?>
+</body>
+</html>
